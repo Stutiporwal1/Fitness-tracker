b30cae7f420af2010e13bacbf436ba647f224a0a
Stuti Porwal <porwalstuti01@gmail.com>
2025-11-29 13:39:08 +0530

    Load and display synthetic exercise tracking data

--- tracking.py
+++ tracking.py
@@ -20,3 +20,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+df = pd.read_csv("/gym_members_exercise_tracking_synthetic_data.csv")
+df.head()
+
